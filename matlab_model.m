48d129913a12f4530649240ffe53e5052f5de3cc
Leonard Wong <leonard.11.wong@gmail.com>
2026-07-18 12:59:50 +0100

    continue making model

--- matlab_model.m
+++ matlab_model.m
@@ -1,3 +1,24 @@
-G = 6.6743 * 10 ^ -11;
-mass1 = 5.972 * 10 ^ 24; % Mass of the Earth in kg
-mass2 = 7.348 * 10 ^ 6; % Mass of the Satellite in kg
+%% Constants
+G = 6.6743e-11;
+mass1 = 5.972e24;      % Earth mass (kg)
+mass2 = 7.348e6;       % Satellite mass (kg)
+
+%% Load and run the Simulink model
+mdl = 'simulink_model';
+load_system(mdl);
+
+simOut = sim(mdl);
+simOut.position.signals.values(1:5,:)
+x = simOut.position.signals.values(:,1);
+y = simOut.position.signals.values(:,2);
+
+figure
+plot(x,y,'LineWidth',1.5)
+hold on
+plot(0,0,'bo','MarkerFaceColor','b','MarkerSize',8)
+
+axis equal
+grid on
+xlabel('x (m)')
+ylabel('y (m)')
+title('Satellite Orbit')
